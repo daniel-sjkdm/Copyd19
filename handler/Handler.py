da3b0ce5731cfcf60bbece0e752ccb97e78c2878
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-24 16:24:03 -0500

    The handler watchs for creating,  deleting and updating events

--- handler/Handler.py
+++ handler/Handler.py
@@ -1,48 +1,59 @@
 from watchdog.events import FileSystemEventHandler
-from rich.logging import RichHandler
 from .utils import IGNORE_DIRS, IGNORE_FILES
+from rich.logging import RichHandler
+from rich.progress import track
+from datetime import datetime
 from gapy.gapy import Gapy
+import logging
 import json
 import os
 import re
-import logging
 
+
+# TODO
+# [ ] Fix deleting a file
 
 
 class FSHandler(FileSystemEventHandler):
+    
     def __init__(self, path):
         self.logger = logging.getLogger(__name__)
         self.path = path
         self.gapy = Gapy()
         self.ignore_dirs = IGNORE_DIRS
         self.ignore_files = IGNORE_FILES
-        if os.path.exists("./filesystem.json"):
-            with open("filesystem.json", "r") as fs:
+        self.fs_path = "handler/filesystem.json"
+        if os.path.exists(self.fs_path):
+            with open(self.fs_path, "r") as fs:
                 self.filesystem = json.load(fs)
         else:
-            self.filesystem = {}
+            self.upload_handler()
 
 
     def on_created(self, event):
         """
             Get the parent id ("if any") and create the file in that directory
         """
-        
+       
         file_name = os.path.basename(event.src_path)
         parent = os.path.dirname(event.src_path)
         parents_id = self.filesystem[parent]["id"]
 
         if event.is_directory:
-            file_id = self.gapy.create_file(file_name, path=parent, parents_id=[parents_id], isFolder=True)
-            self.filesystem[file_name.rstrip("/")] = file_id 
-            self.gapy.logger.info("The directory {} was created with id {}".format(file_name, file_id))
+            if file_name not in self.ignore_dirs:
+                file_id = self.gapy.create_file(file_name, path=parent, parents_id=[parents_id], isFolder=True)
+                self.filesystem[file_name.rstrip("/")] = file_id 
+                self.gapy.logger.info("The directory {} was created with id {}".format(file_name, file_id))
         else:
             if file_name not in self.ignore_files:
                 with open(event.src_path, "w") as empty_file:
                     empty_file.write("\t")
                 file_id = self.gapy.create_file(file_name, path=parent, parents_id=[parents_id])
-                self.filesystem[parent.rstrip("/")]["files"].append({"name": file_name, "id": file_id}) 
+                self.filesystem[parent.rstrip("/")]["files"].append({"name": file_name, "id": file_id})
                 self.gapy.logger.info("The file {} was created with id {}".format(file_name, file_id))
+                print(f"\nFile created: {file_name} at {datetime.now()}")
+
+        self.update_fs()
         
 
     def on_modified(self, event):
@@ -60,6 +71,7 @@ class FSHandler(FileSystemEventHandler):
                 file_id = list(filter(lambda f: f["name"] == file_name, self.filesystem[parent]["files"]))[0]["id"]
                 self.gapy.update_file(file_id, path=parent)
                 self.gapy.logger.info("The file {} was modified, the content was updated".format(file_name, parent))
+                print(f"\nThe file {file_name} was modified and synchronized")
 
 
     def on_deleted(self, event):
@@ -71,19 +83,23 @@ class FSHandler(FileSystemEventHandler):
 
         file_name = os.path.basename(event.src_path)
         self.gapy.logger.warn("The file {} was deleted from local system".format(file_name))
-        choice = input("Do you wan't to remove the file from Google Drive? [y/n]")
+        print(f"\n\033[91m The file {file_name} was deleted from local system \033[0m")
+        choice = input("\nDo you want to remove the file from Google Drive? [y/n]: ")
         
-        while True:
-            if re.fullmatch("[yY]", choice):
-                self.remove_from_filesystem(event.src_path)
-                self.gapy.logger.warning("The file {} was removed from Google Drive".format(file_name))
-                return False
-            elif re.fullmatch("[nN]", choice):
-                self.remove_from_filesystem(event.src_path)
-                self.gapy.logger.warning("The file {} is not removed from Google Drive".format(os.path.basename(event.src_path)))
-                return False
-            else:
-                print("Invalid option, select a valid one")
+        if re.match("[yY]", choice):
+            file_name = os.path.basename(event.src_path)
+            parent = os.path.dirname(event.src_path)
+            file_id = list(filter(lambda f:  f["name"] == file_name, self.filesystem[parent]["files"]))[0]["id"]
+            self.gapy.delete_file(file_id)
+            self.remove_from_filesystem(event.src_path)
+            self.gapy.logger.warning("The file {} was removed from Google Drive".format(file_name))
+            print(f"\n\033[91m The file {file_name} was also removed from Google Drive \033[0m")
+
+        else:
+            self.remove_from_filesystem(event.src_path)
+            self.gapy.logger.warning("\nThe file {} is not removed from Google Drive".format(os.path.basename(event.src_path)))
+        
+        self.update_fs()
 
 
     def upload_handler(self):
@@ -102,33 +118,18 @@ class FSHandler(FileSystemEventHandler):
             else:
                 parents_id = self.filesystem[os.path.dirname(root)]["id"]
                 root_id = self.gapy.create_file(current_dir, path=root, isFolder=True, parents_id=[parents_id])
+                print(f"\033[94m The directory {current_dir} was uploaded \033[0m")
 
             self.filesystem[root.rstrip("/")] = { "id":  root_id, "files": [] }
                 
             if files:
-                for file in files:
-                    if file not in IGNORE_FILES and os.path.getsize(root+"/"+file) > 0:
-                        file_id = self.gapy.create_file(file, path=root, parents_id=[root_id])
-                        self.filesystem[root]["files"].append({ "name": file, "id": file_id})
+                for f in files:
+                    if f not in IGNORE_FILES and os.path.getsize(root+"/"+f) > 0:
+                        file_id = self.gapy.create_file(f, path=root, parents_id=[root_id])
+                        self.filesystem[root]["files"].append({ "name": f, "id": file_id})
+                        print(f"\033[94m The file {f} was uploaded \033[0m")
         
-        with open("./handler/filesystem.json", "a") as fs:
-            json.dump(self.filesystem, fs, indent=4)
-
-
-    def upload_files(self, files):
-        """
-            Receive a list of files and upload them
-        """
-        for file in files:
-            file_name = os.path.basename(file)
-            path = os.path.dirname(file)
-            self.gapy.create_file(file_name, path=path)
-
-
-    def download_handler(self):
-        files = self.gapy.list_files()
-        self.gapy.console.print(files)
-        
+        self.update_fs()
 
 
     def remove_from_filesystem(self, file_path, is_dir=False):
@@ -136,11 +137,29 @@ class FSHandler(FileSystemEventHandler):
             Remove the file from the filesystem and update the filesystem 
             by removing the file from the parent's directory
         """
+        file_name = os.path.basename(file_path)
         if is_dir:
             self.filesystem = self.filesystem.pop(file_path, None)
+
         else:
-            file_name = os.path.basename(file_path)
             parent = os.path.dirname(file_path)
-            self.filesystem[parent]["files"] = list(filter(lambda file: file["name"] != file_name, self.filesystem[parent]["files"]))
-        with open("./handler/filesystem.json", "a") as fs:
-            json.dump(self.filesystem, fs, indent=4)
+            self.filesystem[parent]["files"] = list(filter(lambda f: f["name"] != file_name, self.filesystem[parent]["files"]))
+        
+        self.gapy.logger.info("\nThe file {} was removed from the filesystem".format(file_name))
+
+        self.update_fs()
+
+
+    def create_fs(self):
+        for root, dirs, files in os.walk(self.path):
+            current_dir = os.path.basename(root)
+            
+            if files:
+                for file in files:
+                    self.filesystem[current_dir]["files"] 
+
+
+
+    def update_fs(self):
+        with open(self.fs_path, "w") as f:
+            json.dump(self.filesystem, f, indent=4)
