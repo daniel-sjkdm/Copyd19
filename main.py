e8c42d472658e335ad9b7e74e435eba97885cdd0
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-24 16:24:29 -0500

    Added commands and arguments

--- main.py
+++ main.py
@@ -7,69 +7,98 @@ import time
 import re
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.option(
     "-p",
     "--path",
-    help="Path to the directory to backup/download",
-    required=True,
-    type=click.types.Path(exists=True)
-)
-@click.option(
-    "-w",
-    "--watch",
-    help="Watch the filesystem for events and keep in sync with Google Drive",
-    required=False,
-    is_flag=True
+    help="Path to filesystem to be observed",
+    type=click.types.Path(exists=True),
+    required=True
 )
 @click.option(
     "-wt",
     "--watch-time",
-    help="Time to wait to listen for events",
+    help="Time to be observed",
+    type=click.types.INT,
     required=False,
     default=1
 )
-@click.option(
-    "-f",
-    "--files",
-    help="Files to be downloaded/uploaded",
-    required=False
-)
-@click.option(
-    "-u",
-    "--upload",
-    help="Upload files from path to Google Drive",
-    required=False,
-    is_flag=True
-)
-@click.option(
-    "-f",
-    "--files",
-    help="Files to be uploaded",
-    required=False,
-    multiple=True,
-    type=click.Path(exists=True)
-)
-def main(path, watch, watch_time, upload, files):
+def watch(path, watch_time):
+    """
+        Watch the filesystem for changes to sync to Google Drive
+    """
+
     fsh = FSHandler(path)
-    if watch:
-        fsh.upload_handler()
-        
-        observer = Observer()
-        observer.schedule(fsh, path, recursive=True)
-        observer.start()
+    # create = input("\nDo you want to upload the directory? [y/n]: ")
 
-        try:
-            while True:
-                time.sleep(watch_time)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+    # if re.fullmatch("[yY]", create):
+    #     fsh.upload_handler()
+    # elif re.fullmatch("[nN]", create):
+    #     pass
+    #     if not fsh.filesystem:
+    #         print("Creating the filesystem")
+    #         # fsh.create_fs()
+    # else:
+        # raise Exception("\nInvalid choice")
+    
+    click.secho("\nObserving {} every {} seconds".format(path, watch_time), fg="green")
+    observer = Observer()
+    observer.schedule(fsh, path, recursive=True)
+    observer.start()
 
-    elif upload:
-        fsh.upload_files(files)
+    try:
+        while True:
+            time.sleep(watch_time)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
-        
+
+@main.command()
+def listfiles():
+    """
+        Display the available files at Google Drive
+    """
+    gapy = Gapy()
+    gapy.list_files(_print=True)
+
+
+@main.command()
+@click.option(
+    "-t",
+    "--to",
+    help="Where to download the files",
+    type=click.types.Path(exists=True),
+    required=True
+)
+def download(to):
+    """
+        Download the selected files to the target directory
+    """
+    print(f"\nDownloading files to {to}")
+
+
+@main.command()
+@click.argument(
+    "files",
+    nargs=-1,
+    type=click.types.Path(exists=True)
+)
+def upload(files):
+    """
+        Upload the files to google drive
+    """
+    gapy = Gapy()
+    with click.progressbar(files, label="Uploading files") as bar:
+        for f in bar:
+            gapy.create_file(f, path=os.path.dirname(f))
+
+
 
 if __name__ == "__main__":
     main()
